373b40cc263b90a75d2d766d0286ea0733cb81a8
nickzpower <nickzpower2@gmail.com>
2021-04-23 13:07:19 +0300

    add loader component

---
+++ soa/front-end/src/components/loader/Loader.js
@@ -0,0 +1,11 @@
+import './Loader.css'
+import React from 'react';
+
+const Loader = ()=>{
+    return (
+    <div>
+    <div className = 'loader-text'> Loading...</div>
+    <div className="main-loader"></div>
+    </div>);
+}
+export default Loader;
