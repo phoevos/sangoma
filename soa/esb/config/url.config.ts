b91e4e84c0ff0e0059ef881ba5e8ed0bc8e8186c
nickzpower <nickzpower2@gmail.com>
2021-07-09 18:12:37 +0300

    Deployment: ESB port change for heroku

--- soa/esb/config/url.config.ts
+++ soa/esb/config/url.config.ts
@@ -1,1 +1,2 @@
-export const BASE_URL = 'https://sangoma-soa.herokuapp.com/'
+export const  BASE_URL = 'https://sangoma-soa.herokuapp.com/'
+export const PORT = 4000
--- soa/esb/src/main.ts
+++ soa/esb/src/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { PORT } from 'config/url.config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  await app.listen(4000);
+  const port = process.env.PORT || PORT
+  await app.listen(port);
 }
 bootstrap();
